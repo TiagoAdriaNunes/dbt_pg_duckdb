2fbf029e1547f1ff394941353d3968a046f7389d
Tiago Adria Nunes <tiagoadrianunes@gmail.com>
2026-06-20 04:47:17 -0300

    feat: set pg as default database in duckdb-cli

--- scripts/duckdb_cli.sh
+++ scripts/duckdb_cli.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-duckdb -cmd "ATTACH 'host=${POSTGRES_HOST:-localhost} port=${POSTGRES_PORT:-5432} dbname=${POSTGRES_DB:-analytics} user=${POSTGRES_USER:-postgres} password=${POSTGRES_PASSWORD:-duckdb}' AS pg (TYPE POSTGRES);"
+duckdb \
+  -cmd "ATTACH 'host=${POSTGRES_HOST:-localhost} port=${POSTGRES_PORT:-5432} dbname=${POSTGRES_DB:-analytics} user=${POSTGRES_USER:-postgres} password=${POSTGRES_PASSWORD:-duckdb}' AS pg (TYPE POSTGRES);" \
+  -cmd "USE pg;"
